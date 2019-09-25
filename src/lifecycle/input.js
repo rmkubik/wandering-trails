dc8ab69c8483aa4b0940e715739c702b8a3bdd08
Ryan Kubik <rmkubik@me.com>
2019-09-24 23:58:13 -0700

    Create Mouse class

--- src/lifecycle/input.js
+++ src/lifecycle/input.js
@@ -11,10 +11,33 @@ export class Keyboard {
 
   addListener(key, handler) {
     if (!this.listenerMap[key]) {
-      // no exisiting handler
+      // no existing handler
       this.listenerMap[key] = [];
     }
 
     this.listenerMap[key].push(handler);
+  }
+}
+
+export class Mouse {
+  constructor() {
+    this.listenerMap = {};
+    this.LEFT_BUTTON = 0;
+    this.RIGHT_BUTTON = 2;
+
+    document.addEventListener("mousedown", event => {
+      if (this.listenerMap[event.button]) {
+        this.listenerMap[event.button].forEach(listener => listener(event));
+      }
+    });
+  }
+
+  addListener(button, handler) {
+    if (!this.listenerMap[button]) {
+      // no existing handler
+      this.listenerMap[button] = [];
+    }
+
+    this.listenerMap[button].push(handler);
   }
 }
