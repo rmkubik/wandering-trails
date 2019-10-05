8bf2c683e27d874e51225d5d41e8886df1354ab6
Ryan Kubik <rmkubik@me.com>
2019-10-04 19:46:39 -0700

    getLocationFromXY was inverted

--- src/location/getLocationFromXY.js
+++ src/location/getLocationFromXY.js
@@ -4,8 +4,8 @@ export default function getLocationFromXY({ x, y }) {
   const [map] = document.getElementsByClassName("map");
   const { offsetLeft, offsetTop } = map;
 
-  const row = Math.floor((x - offsetLeft) / config.tileWidth);
-  const col = Math.floor((y - offsetTop) / config.tileHeight);
+  const row = Math.floor((y - offsetTop) / config.tileHeight);
+  const col = Math.floor((x - offsetLeft) / config.tileWidth);
 
   return { row, col };
 }
